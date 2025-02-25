185b3bf5e3ba94190b4bfa4a654dc0a8fbde45c1
ArcaneTomes <annelise.leonard2@gmail.com>
2025-02-24 21:12:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function openForm() {
-    document.getElementById("contact-form").style.display = "block";
-  }
+ /*
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  */
+
   
-  function closeForm() {
-    document.getElementById("contact-form").style.display = "none";
-  }
